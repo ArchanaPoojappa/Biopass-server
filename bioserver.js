74c93315556d75f38da03a213c79082382437908
ArchanaPoojappa <120667958+ArchanaPoojappa@users.noreply.github.com>
2024-05-14 12:57:29 +0530

    Add files via upload

---
+++ bioserver.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const socket = require("socket.io");
+const fs = require("fs");
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cors());
+
+const userRouter = require("./src/routes/bioRoutes");
+
+app.use("/biopass", userRouter);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+const server = app.listen(port);
+
+const io = socket(server, { cors: { origin: "*", methods: ["GET", "POST"] } });
+const clients = io.sockets.sockets;
+if (clients.size > 0) {
+  const client = clients.values().next().value;
+  const wsServerUrl = client.conn.remoteAddress + ":" + client.conn.remotePort;
+  console.log("WebSocket server URL:", wsServerUrl);
+}
+io.on("connection", (socket) => {
+  console.log("New socket connection: " + socket.id);
+  socket.on("checkSocket", () => {
+    io.emit("checkSocket", "worked Sockets");
+  });
+
+  socket.on("authenticate", (data) => {
+    io.emit("authenticate", data);
+  });
+  socket.on("authResult", (data) => {
+    io.emit("authResult", data);
+  });
+});
